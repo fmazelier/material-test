c6735fbd24ebd6a6e2c216156f17777877fbef0e
Florian Mazelier <f.mazelier@icloud.com>
2026-05-02 22:27:39 +0200

    fix: Fetch config.json directly from root path

--- src/app/core/services/config.service.ts
+++ src/app/core/services/config.service.ts
@@ -1,16 +1,13 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 import { AppConfig } from '@core/models/config.model';
 
 @Injectable({ providedIn: 'root' })
 export class ConfigService {
-  private readonly baseHref = inject(APP_BASE_HREF);
-
   private config!: AppConfig;
 
   async loadConfig(): Promise<void> {
-    const response = await fetch(`${this.baseHref}config.json`);
+    const response = await fetch('config.json');
     if (!response.ok) {
       throw new Error(
         "[Config] échec du chargement du fichier de configuration nécessaire au bon fonctionnement de l'app"
