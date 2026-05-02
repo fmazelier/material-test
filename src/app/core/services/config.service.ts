80d73eeffddc9d8a7bccc1f4d1f1308437f69f16
Florian Mazelier <f.mazelier@icloud.com>
2026-05-02 22:25:52 +0200

    fix: Fetch config.json relative to base href

--- src/app/core/services/config.service.ts
+++ src/app/core/services/config.service.ts
@@ -1,13 +1,16 @@
-import { Injectable } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { inject, Injectable } from '@angular/core';
 
 import { AppConfig } from '@core/models/config.model';
 
 @Injectable({ providedIn: 'root' })
 export class ConfigService {
+  private readonly baseHref = inject(APP_BASE_HREF);
+
   private config!: AppConfig;
 
   async loadConfig(): Promise<void> {
-    const response = await fetch('/config.json');
+    const response = await fetch(`${this.baseHref}config.json`);
     if (!response.ok) {
       throw new Error(
         "[Config] échec du chargement du fichier de configuration nécessaire au bon fonctionnement de l'app"
