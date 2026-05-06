928a684d27d525466176bc03ec16cf99c40b32b6
Florian Mazelier <47300733+fmazelier@users.noreply.github.com>
2026-05-06 16:36:51 +0200

    refactor: use native css/tailwind @container

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -1,57 +1,34 @@
 import { DatePipe, NgOptimizedImage } from '@angular/common';
-import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  inject,
-  signal,
-} from '@angular/core';
-
-const FOOTER_MIN_WIDTH_FOR_ROW_LAYOUT = 400;
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   imports: [NgOptimizedImage, DatePipe],
   template: `
     <footer
-      #footer
-      class="p-4 border-t border-on-surface/20 flex"
-      [class.flex-col]="isSmall()"
-      [class.items-center]="isSmall()"
-      [class.gap-2]="isSmall()"
-      [class.flex-row]="!isSmall()"
-      [class.justify-between]="!isSmall()"
+      class="p-4 border-t border-on-surface/20
+      flex flex-col items-center gap-2
+      @[400px]:flex-row @[400px]:justify-between"
     >
-      <p [class.order-2]="isSmall()" class="font-medium">
-        Copyright © Safran Aircraft Engines {{ currentDate | date: 'y' }}
-      </p>
       <img
-        [class.order-1]="isSmall()"
+        class="@[400px]:order-2"
         ngSrc="logo-safran.webp"
         width="50"
         height="55"
         alt="Logo de safran"
       />
+      <p class="font-medium @[400px]:order-1">
+        Copyright © Safran Aircraft Engines {{ currentDate | date: 'y' }}
+      </p>
     </footer>
   `,
   styles: `
     :host {
-      display: block;
+      container-type: inline-size;
     }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent implements AfterViewInit {
-  private readonly host = inject(ElementRef);
-  private readonly observer = new ResizeObserver(([entry]) => {
-    this.isSmall.set(entry.contentRect.width < FOOTER_MIN_WIDTH_FOR_ROW_LAYOUT);
-  });
-
-  protected readonly isSmall = signal(false);
+export class FooterComponent {
   protected readonly currentDate = new Date();
-
-  ngAfterViewInit(): void {
-    this.observer.observe(this.host.nativeElement);
-  }
 }
