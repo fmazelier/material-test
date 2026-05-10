ccf4af6d8cc2844e6639a41b812b1ac22c73cc61
Florian Mazelier <f.mazelier@icloud.com>
2026-05-10 06:18:15 +0200

    refactor: move safran logo to images directory

--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -15,7 +15,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
         href="https://www.safran-group.com/companies/safran-aircraft-engines"
         target="_blank"
       >
-        <img ngSrc="logo-safran.webp" width="32" height="35" alt="Logo de safran" />
+        <img ngSrc="images/logo-safran.webp" width="32" height="35" alt="Logo de safran" />
       </a>
       <p class="font-medium @[400px]:order-1">
         Copyright © Safran Aircraft Engines {{ currentDate | date: 'y' }}
