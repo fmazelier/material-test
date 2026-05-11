f22ce02682a033dddb1bf8c55074491ffb046ab7
Florian Mazelier <47300733+fmazelier@users.noreply.github.com>
2026-05-11 10:16:24 +0200

    fix

--- src/app/shared/components/icon/icon.component.ts
+++ src/app/shared/components/icon/icon.component.ts
@@ -5,6 +5,7 @@ import {
   input,
   ViewEncapsulation,
 } from '@angular/core';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 import { LucideDynamicIcon, LucideIcon } from '@lucide/angular';
 
@@ -13,8 +14,14 @@ const DEFAULT_ICON_SIZE = 24;
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'mat-icon[appIcon]',
-  imports: [LucideDynamicIcon],
-  template: `<svg [lucideIcon]="icon()" [size]="svgSize()" [strokeWidth]="2"></svg>`,
+  imports: [LucideDynamicIcon, MatProgressSpinner],
+  template: `
+    @if (loading()) {
+      <mat-spinner [diameter]="svgSize()" />
+    } @else {
+      <svg [lucideIcon]="icon()" [size]="svgSize()" [strokeWidth]="2"></svg>
+    }
+  `,
   encapsulation: ViewEncapsulation.None,
   host: {
     class: 'mat-icon',
@@ -29,6 +36,7 @@ const DEFAULT_ICON_SIZE = 24;
 export class IconComponent {
   icon = input.required<LucideIcon>();
   size = input<number>();
+  loading = input<boolean>(false);
 
   protected explicitSize = computed(() => this.size() ?? null);
   protected svgSize = computed(() => this.size() ?? DEFAULT_ICON_SIZE);
